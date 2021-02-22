254df5aa85420b32257e1f5c84f2b8864d5f336e
Svetlana Kudryavtseva <springinmypalms@gmail.com>
2021-02-22 16:24:38 +0300

    test_

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import "./assets/scss/main.scss";
+//-document.querySelector('.js-menu-button').addEventListener('click', (e) => {
+//-    if (e.target.getAttribute('class') === 'menu-list-element-a')
+//-       alert('class');
+//-}, false);
